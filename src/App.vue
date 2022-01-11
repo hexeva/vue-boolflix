bfd8562fd96b504d947152b9cc538664ecfe5fc2
hexeva <patrick79.g@gmail.com>
2022-01-11 15:13:38 +0100

    started

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header />
+    <main>MAIN</main>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import axios from 'axios';
+// import HelloWorld from "./components/HelloWorld.vue";
+ import Header from "./components/Header.vue";
+
+
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    Header,
   },
+
+methods:{
+  
+},
+
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './style/general';
+
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <input id="Search" type="text" placeholder="Cerca">
+        <button @click="callApi">Invia ricerca</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name:'Header',
+
+    methods:{
+
+        callApi: function(){
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params:{
+                    api_key:'3b8e65acb65bbd04677cf76f0c18bc6d',
+                    query:'terminator'
+                }
+            })
+            .then((response) => {
+                console.log(response.data.results);
+            });
+        }
+        
+        
+    },
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
